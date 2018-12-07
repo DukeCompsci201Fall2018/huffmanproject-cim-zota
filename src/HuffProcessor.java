39e5ecbf7cf2bd2071ad771a6441b3a98a0bbc2b
Jacob Cimerberg <Jacob.cimerberg@duke.edu>
2018-12-06 19:04:55 -0500

    make tree from counts

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,3 +1,4 @@
+import java.util.PriorityQueue;
 
 /**
  * Although this class has a history of several years,
@@ -70,7 +71,20 @@ public class HuffProcessor {
 	}
 
 	private HuffNode makeTreeFromCounts(int[] counts) {
+		PriorityQueue<HuffNode> pq = new PriorityQueue<>();
 		
+		for (int i = 0; i < counts.length; i++) {
+			if (counts[i] >0) pq.add(new HuffNode(i, counts[i], null, null));
+		}
+		
+		while (pq.size() > 1) {
+			HuffNode left = pq.remove();
+			HuffNode right = pq.remove();
+			HuffNode t = new HuffNode(0,left.myWeight + right.myWeight, left,right);
+			pq.add(t);
+		}
+		HuffNode root = pq.remove();
+		return root;
 	}
 
 	private int[] readForCounts(BitInputStream in) {
